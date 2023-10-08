fb0610c8a8f16c88c37ca7a8b11d2ba4f22ef116
thayscasado <thayscasado@hotmail.com>
2023-10-08 15:00:40 -0700

    📦✨ Installed 'styled-components' and related types
    Installed 'styled-components' and added type dependencies ('@types/styled-components' and '@types/styled-components-react-native'). Additionally, included a basic 'styles.ts' file in the 'home' directory for testing.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Text } from 'react-native';
+import { Container } from './styles';
 export function Home() {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Home</Text>
-    </View>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
